c4799c92d011fec99359e84b8b35f10c7da5ef8b
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-08-28 16:30:14 -0300

    passport

--- controllers/users/signIn.js
+++ controllers/users/signIn.js
@@ -1,30 +1,23 @@
-import User from '../../models/User.js';
+import User from "../../models/User.js";
 
-export default async (req, res, next) => {
-  try {
-    const user = await User.findOneAndUpdate(
-      { email: req.body.email },
-      { password: req.body.password, online: true },
-      { new: true }
-    );
-
-    if (user) {
-      delete user.password;
-      return res.status(200).json({
-        message: "You have successfully logged in!",
-        response: {
-          user: user.email,
-          photo:user.photo,
-          token: req.token
-        }
-      });
-    } else {
-      return res.status(400).json({
-        response: null,
-        message: "NOT Logged in!"
-      });
+export default async(req,res,next)=> {
+    try {
+        let one = await User.findOneAndUpdate(
+            {email: req.body.email},
+            {online: true},
+            {new: true}
+        )
+        delete one.password
+        return res.status(200).json({
+            success:true,
+            message:'user signed in!',
+            response: {
+                user: one.email,
+                photo: one.photo,
+                token: req.token
+            }
+        })
+    } catch (error) {
+        return next()
     }
-  } catch (error) {
-    return next(error);
-  }
-};
+}
--- middlewares/passwordIsOK.js
+++ middlewares/passwordIsOK.js
@@ -1,18 +1,18 @@
-import User from "../models/User.js"
-import bcryptjs from "bcryptjs"
+import User from "../models/User.js";
+import bcrypt from 'bcrypt'
 
-export default async(req,res,next)=> {
+export default async(req, res, next)=> {
     try {
-        let one = await User.findOne({ email:req.body.email })
+        let one = await User.findOne({ email:req.body.email})
         let mongo_user_password = one.password
         let form_password = req.body.password
-        let compare = bcryptjs.compareSync(form_password,mongo_user_password)
+        let compare = bcrypt.compareSync(form_password, mongo_user_password)
         if (compare) {
             return next()
         }
         return res.status(400).json({
-            success:false,
-            message:'invalid credentials!'
+            response: null, 
+            message: 'Invalid credentials!'
         })
     } catch (error) {
         return next(error)
