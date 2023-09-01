b6e8803ae0336f5e168adc93b44166058befde15
Gustavo alexander Vasquez More <123557057+Gustavo-Alexander-Vasquez-More@users.noreply.github.com>
2023-09-01 12:09:21 -0300

    detalles

---
+++ controllers/mangas/readPagination.js
@@ -0,0 +1,42 @@
+import Manga from '../../models/Manga.js'
+
+export default async(req, res)=>{
+    try {
+        let pagination = { page: 1, limit: 8 };
+        if (req.body.page) {
+          pagination.page = req.body.page;
+        }
+        if (req.body.limit) {
+          pagination.limit = req.body.limit;
+        }
+        const totalDocuments = await Manga.find(req.body).countDocuments();
+        pagination.page = parseInt(pagination.page);
+        pagination.limit = parseInt(pagination.limit);
+    
+        const read_mangas = await Manga.find(req.body)
+          .select("title cover_photo ")
+          .populate({
+            path: 'category_id',
+          select: 'name -_id',
+        
+        })
+          .skip(pagination.page > 0 ? (pagination.page - 1) * pagination.limit : 0)
+          .limit(pagination.limit > 0 ? pagination.limit : 0);
+    
+        const nextPageHasDocuments = pagination.page * pagination.limit < totalDocuments;
+        const nextPage = nextPageHasDocuments ? pagination.page + 1 : null;
+        const currentPage = pagination.page;  // Agregar el número de página actual
+    
+        
+        return res.status(200).json({
+          success: true,
+          response: {
+            mangas:read_mangas,
+            next: nextPage,
+          currentPage: currentPage
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+}
